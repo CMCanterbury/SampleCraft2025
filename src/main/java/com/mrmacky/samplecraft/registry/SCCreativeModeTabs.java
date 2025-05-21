c11793cef0f723002f81c9615863117280dc14c0
CMCanterbury <cmacky@canterburyschool.org>
2025-05-21 14:01:55 -0400

    Added a new item.

--- src/main/java/com/mrmacky/samplecraft/registry/SCCreativeModeTabs.java
+++ src/main/java/com/mrmacky/samplecraft/registry/SCCreativeModeTabs.java
@@ -3,9 +3,14 @@ package com.mrmacky.samplecraft.registry;
 import com.mrmacky.samplecraft.SampleCraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+@EventBusSubscriber(modid = SampleCraft.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class SCCreativeModeTabs {
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, SampleCraft.MOD_ID);
@@ -14,6 +19,13 @@ public class SCCreativeModeTabs {
 
     public static void register(IEventBus bus) {
         CREATIVE_MODE_TABS.register(bus);
+    }
+
+    @SubscribeEvent
+    private static void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(SCItems.EXAMPLE_ITEM);
+        }
     }
 
 }
--- src/main/java/com/mrmacky/samplecraft/registry/SCItems.java
+++ src/main/java/com/mrmacky/samplecraft/registry/SCItems.java
@@ -1,14 +1,32 @@
 package com.mrmacky.samplecraft.registry;
 
 import com.mrmacky.samplecraft.SampleCraft;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
+import java.util.Optional;
 
 public class SCItems {
 
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(SampleCraft.MOD_ID);
 
     //Items go here
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(200)
+                    .fireResistant()
+                    .food(
+                            new FoodProperties(10, 2, true, 5,
+                                    Optional.of(Items.DIAMOND.getDefaultInstance()),
+                                    List.of(new FoodProperties.PossibleEffect(() -> new MobEffectInstance(MobEffects.DARKNESS, 1000), 1))))
+                    ));
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);
